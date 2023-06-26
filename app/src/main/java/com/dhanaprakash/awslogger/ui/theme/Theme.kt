3132fcae69323412bc5a47ae67240ee0ce18fcc1
Dhana Prakash M <dhanaprakash@mallow-tech.com>
2023-06-26 12:00:49 +0530

    $Project setup

---
+++ app/src/main/java/com/dhanaprakash/awslogger/ui/theme/Theme.kt
@@ -0,0 +1,39 @@
+package com.dhanaprakash.awslogger.ui.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+
+private val DarkColorScheme = lightColors(
+    primary = Purple80,
+    secondary = PurpleGrey80,
+)
+
+private val LightColorScheme = darkColors(
+    primary = Purple40,
+    secondary = PurpleGrey40,
+
+    /* Other default colors to override
+    background = Color(0xFFFFFBFE),
+    surface = Color(0xFFFFFBFE),
+    onPrimary = Color.White,
+    onSecondary = Color.White,
+    onBackground = Color(0xFF1C1B1F),
+    onSurface = Color(0xFF1C1B1F),
+    */
+)
+
+@Composable
+fun AwsLoggerTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+    val colors = if (darkTheme) DarkColorScheme else LightColorScheme
+    MaterialTheme(
+        colors = colors,
+        typography = Typography,
+        content = content
+    )
+}
